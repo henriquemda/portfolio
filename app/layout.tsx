c48c8bf7f841f9d001ed58d30af92f6df45cad59
Henrique <henrique.dallapicula@gmail.com>
2024-06-08 16:39:43 -0500

    Update layout.tsx
    Change title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Rebirt 🔥 Unleash your digital potential.",
+  description: "Transforming Concepts into Seamless User Experiences",
 };
 
 export default function RootLayout({
