0bbde3dbbab30751f0e2b620a9b69e8161c69ae2
Henrique <henrique.dallapicula@gmail.com>
2024-06-09 01:46:20 -0500

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -61,7 +61,7 @@ const Hero = () => {
                 className="lg:py-4 lg:px-3 py-2 px-3 mr-3 text-xs lg:text-base opacity-50 
                 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
               >
-                E-commerce
+                Tráfico pago
             </span>
 
             <span
