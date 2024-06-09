d7a7b69577d9c6186afdcfaf08d1cc0cf4655f6b
Henrique <henrique.dallapicula@gmail.com>
2024-06-09 13:08:44 -0500

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -81,7 +81,7 @@ export const projects = [
   {
     id: 2,
     title: "Todo Surf Shop E-commerce",
-    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    des: "Un E-commerce cautivador que captura la esencia de la cultura del surf de Perú, brindando una experiencia de compra en línea inmersiva.",
     img: "/p2.svg",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
     link: "/ui.yoom.com",
