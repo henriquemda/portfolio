0da267eb36302553443174c589027693f45efe04
Henrique <henrique.dallapicula@gmail.com>
2024-06-09 01:25:57 -0500

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -72,7 +72,7 @@ const Hero = () => {
             </span>
 
             <span
-                className="lg:py-4 lg:px-3 py-2 px-3 mr-3 text-xs lg:text-base opacity-50 
+                className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
                 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
               >
                 Inteligencia Artificial
