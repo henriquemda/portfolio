ea0b2ec2d15952b896764ae3f16e54b33d6a8023
Henrique <henrique.dallapicula@gmail.com>
2024-06-08 23:55:38 -0500

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -1,8 +1,8 @@
 export const navItems = [
-  { name: "About", link: "#about" },
-  { name: "Projects", link: "#projects" },
-  { name: "Testimonials", link: "#testimonials" },
-  { name: "Contact", link: "#contact" },
+  { name: "Nosotros", link: "#about" },
+  { name: "Proyectos", link: "#projects" },
+  { name: "Testimonios", link: "#testimonials" },
+  { name: "Contacto", link: "#contact" },
 ];
 
 export const gridItems = [
