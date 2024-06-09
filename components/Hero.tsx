0e980699b26fe683772034e2f8166efd2f8b22a8
Henrique <henrique.dallapicula@gmail.com>
2024-06-09 01:19:12 -0500

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -56,27 +56,28 @@ const Hero = () => {
             className="text-center text-[30px] md:text-4xl lg:text-5xl"
           />
 
-          <span
-              className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
-              lg:opacity-100 rounded-lg text-center bg-[#10132E]"
-            >
-              E-commerce
-          </span>
+          <div className="items-center justify-center">
+            <span
+                className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+              >
+                E-commerce
+            </span>
 
-           <span
-              className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
-              lg:opacity-100 rounded-lg text-center bg-[#10132E]"
-            >
-              Automatización
-          </span>
+            <span
+                className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+              >
+                Automatización
+            </span>
 
-           <span
-              className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
-              lg:opacity-100 rounded-lg text-center bg-[#10132E]"
-            >
-              Inteligencia Artificial
-          </span>
-
+            <span
+                className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+              >
+                Inteligencia Artificial
+            </span>
+          </div>
           <a href="#about">
             <MagicButton
               title="Show our work"
