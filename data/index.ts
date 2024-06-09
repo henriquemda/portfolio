f897d2d1e29e025bd28e8867a010a7fb3629995e
Henrique <henrique.dallapicula@gmail.com>
2024-06-09 00:01:46 -0500

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -8,7 +8,7 @@ export const navItems = [
 export const gridItems = [
   {
     id: 1,
-    title: "We prioritize client collaboration, fostering open communication ",
+    title: "Priorizamos la colaboración con el cliente, fomentando la comunicación abierta ",
     description: "",
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
@@ -18,7 +18,7 @@ export const gridItems = [
   },
   {
     id: 2,
-    title: "We're very flexible with time zone communications",
+    title: "Somos muy flexibles con las comunicaciones de zona horaria.",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
     imgClassName: "",
@@ -28,7 +28,7 @@ export const gridItems = [
   },
   {
     id: 3,
-    title: "Our tech stack",
+    title: "Nuestra tech stack",
     description: "We constantly try to improve",
     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
     imgClassName: "",
@@ -38,7 +38,7 @@ export const gridItems = [
   },
   {
     id: 4,
-    title: "Tech enthusiasts with a passion for development.",
+    title: "Entusiastas de la tecnología con pasión por el desarrollo.",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
@@ -49,7 +49,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building an AI Saas for Perú",
+    title: "Actualmente construyendo un Saas de IA para Perú",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
